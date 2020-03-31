99e31fa36dccf6d57ce54ecdcaaa525f13c14642
David Domínguez Zapata <david.dominguezz@um.es>
2020-03-31 12:40:39 +0200

    David Dom. modificación de prog.c

--- src/prog.c
+++ src/prog.c
@@ -7,9 +7,21 @@ int es_par(int num){
 		return 0;
 }
 
+int es_impar(int num){
+    if(num % 2 != 0)
+        return 1;
+    else
+        return 0;
+}
+
 int main(void){
 	printf("¿Es 32 un número par?\n");
 	if(es_par(32))
+		printf("Yes\n");
+	else
+		printf("No\n");
+    printf("¿Es 33 un número impar?\n");
+	if(es_impar(33))
 		printf("Yes\n");
 	else
 		printf("No\n");
