ab43a720c2c2cc248e49aee44c4f5ddac37aa0b2
Sergio <sergio.martineza@um.es>
2020-04-06 16:36:49 +0200

    Dos funciones mas

--- src/prog.c
+++ src/prog.c
@@ -7,7 +7,19 @@ int es_par(int num){
 		return 0;
 }
 
+void hola(){
+	printf("Hola\n");
+	return;
+}
+
+void respuesta(){
+	printf("Vamos a dar la respuesta a la siguiente pregunta\n");
+	return;
+}
+
 int main(void){
+	hola();
+	respuesta();
 	printf("¿Es 32 un número par?\n");
 	if(es_par(32))
 		printf("Yes\n");
